0ed18486696bbf4ae9096bc60c0ed76ffa7cc66d
irasemarivera <irasema.rivera@hotmail.com>
2022-07-12 20:34:26 +0200

    Changing message when loading

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 class Error extends Component {
     render() {
         return (
-            <div><h3>Usuario no autorizado. Contacta al administrador: <a href='mailto:{process.env.REACT_APP_ADMIN_EMAIL}'>{process.env.REACT_APP_ADMIN_EMAIL}</a></h3></div>
+            <div><h3>Cargando... </h3></div>
         );
     }
 }
